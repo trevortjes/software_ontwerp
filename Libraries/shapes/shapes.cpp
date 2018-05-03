01f2de8342105ee296274836b11d9e5e65f80f14
matboy1200 <matboy1200@DESKTOP-HSQL3CU.wifi.hu.nl>
2018-05-03 13:47:20 +0200

    lol

--- Libraries/shapes/shapes.cpp
+++ Libraries/shapes/shapes.cpp
@@ -9,7 +9,7 @@
 //GODVERDIKKIE, HET WERKT NIET? ALLEMAAL FOUTJES!h
 PixelBuffer* ppBuf;
 
-uint8_t setPixelBuffer(PixelBuffer* pb){
+uint8_t setPixelBuffer(Pixel Buffer* pb){
 	ppBuf=pb;
 	return 0;
 }
--- Libraries/shapes/shapes.h
+++ Libraries/shapes/shapes.h
@@ -9,7 +9,7 @@
 #include "pixelBuffer.h"
 
 uint8_t setPixelBuffer(PixelBuffer* pb);
-uint8_t drawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t col);
+uint8_t drawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t col );
 uint8_t fill(uint8_t shape, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3, uint16_t col);
 uint8_t drawRect(uint16_t x3, uint16_t y3, uint16_t x2, uint16_t y2, uint8_t col, uint16_t fill);
 uint8_t drawTri(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3, uint16_t col, uint16_t fill);
