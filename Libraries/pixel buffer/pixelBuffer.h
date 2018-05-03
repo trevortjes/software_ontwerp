9d1cec8c073c1e9903ddc3ef0495a6db450fe9ba
matboy1200 <matboy1200@DESKTOP-HSQL3CU.wifi.hu.nl>
2018-05-03 12:16:51 +0200

    pixelbuffer fix

--- Libraries/pixel buffer/pixelBuffer.h
+++ Libraries/pixel buffer/pixelBuffer.h
@@ -4,8 +4,7 @@
  *  Created on: May 1, 2018
  *      Author: Matthijs Uit den Bogaard
  */
-#include "stm32f4xx.h"
-#include "stm32f4_discovery.h"
+
 
 #ifndef PROGRAMMA_LIBRARIES_PIXEL_BUFFER_PIXELBUFFER_H_
 #define PROGRAMMA_LIBRARIES_PIXEL_BUFFER_PIXELBUFFER_H_
@@ -14,14 +13,14 @@
 
 class PixelBuffer {
 	private:
-		uint_8 color[255]={};
-		uint_16 position[255][2]={};
-		uint_8 size=0;
+		uint8_t color[255]={};
+		uint16_t position[255][2]={};
+		uint8_t size=0;
 		void clearBuffer();
 	public:
 		PixelBuffer();
-		uint_8 add(uint_8 col, uint_16 x, uint_16 y);
-		uint_8 push();
+		uint8_t add(uint8_t col, uint16_t x, uint16_t y);
+		uint8_t push();
 
 };
 
