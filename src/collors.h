08a96efedbd2a3a849788af89bfcd11b9103d2c6
matboy1200 <matboy1200@DESKTOP-LIF89KP>
2018-05-16 11:17:07 +0200

    lol

---
+++ src/collors.h
@@ -0,0 +1,30 @@
+/*
+ * collors.h
+ *
+ *  Created on: May 15, 2018
+ *      Author: matboy1200
+ */
+
+#ifndef COLLORS_H_
+#define COLLORS_H_
+
+
+//--------------------------------------------------------------
+// color designation
+// 8bit color (R3G3B2)
+// Red   (3bit) -> Bit7-Bit5
+// Green (3bit) -> Bit4-Bit2
+// Blue  (2bit) -> Bit1-Bit0
+//--------------------------------------------------------------
+#define  BLACK          0x00
+#define  BLUE           0x03
+#define  GREEN          0x1C
+#define  RED            0xE0
+#define  WHITE          0xFF
+
+#define  CYAN           0x1F
+#define  MAGENTA        0xE3
+#define  YELLOW         0xFC
+
+
+#endif /* COLLORS_H_ */
