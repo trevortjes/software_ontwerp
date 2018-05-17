e8eea569660e6800ffcd593ceec9b68f997d94df
matboy1200 <matboy1200@DESKTOP-HSQL3CU.wifi.hu.nl>
2018-05-17 11:34:20 +0200

    wait.h toegevoegd

---
+++ Libraries/Wait/wait.h
@@ -0,0 +1,15 @@
+/*
+ * wait.h
+ *
+ *  Created on: May 17, 2018
+ *      Author: matboy1200
+ */
+
+#ifndef WAIT_H_
+#define WAIT_H_
+
+uint8_t waitInit();
+uint8_t wait(uint16_t ms); //maximaal een minuut wait
+
+
+#endif /* WAIT_H_ */
