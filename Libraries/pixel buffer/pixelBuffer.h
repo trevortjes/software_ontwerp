01c6e5e8abdeeb9918da8e19a0f88dcb88b992b3
matboy1200 <matboy1200@DESKTOP-HSQL3CU.wifi.hu.nl>
2018-05-31 12:55:41 +0200

    doxygen pixelbuffer

--- Libraries/pixel buffer/pixelBuffer.h
+++ Libraries/pixel buffer/pixelBuffer.h
@@ -1,8 +1,10 @@
-/*
- * pixelBuffer.h
+/**
+ * *************************************************************************************
+ *@file pixelBuffer.h
+ *@brief Buffer met aan te passen pixelwaarden
+ *@author Matthijs Uit den Bogaard
  *
- *  Created on: May 1, 2018
- *      Author: Matthijs Uit den Bogaard
+ *Klasse die de aan te passen pixels opslaat in een buffer en dan wanneer alle commands uitgevoerd zijn of het wait commando is aangeroepen de pixels naar het scherm pusht
  */
 #ifndef PIXELBUFFER_H
 #define PIXELBUFFER_H
@@ -11,15 +13,40 @@
 #include "stm32f4_discovery.h"
 
 class PixelBuffer {
-	private:
-		uint8_t color[256];
-		uint16_t position[256][2];
-		uint16_t size;
-		void clearBuffer();
-	public:
-		PixelBuffer();
-		uint8_t add(uint8_t col, uint16_t x, uint16_t y);
-		uint8_t push();
+private:
+	/**
+	 * @bief Array met de kleuren voor de pixels
+	 */
+	uint8_t color[256];
+	/**
+	 * @brief 2 demensionaal array met de coördinaten van de pixels
+	 */
+	uint16_t position[256][2];
+	/**
+	 * @brief de lengte van de buffer
+	 */
+	uint16_t size;
+	void clearBuffer();
+public:
+	/**
+	 * @brief default constructor van de pixelbuffer
+	 */
+	PixelBuffer();
+	/**
+	 * @brief functie add voegt een pixel toe aan de pixel buffer
+	 * @param col De kleur van de pixel
+	 * @param x De x positie van de pixel
+	 * @param y de y positie van de pixel
+	 * @return de errorcode als het goed gaat wordt 0 gereturnt
+	 *
+	 * De functie add voegt een pixel toe aan de pixelbuffer met de meegegeven kleur x en y positie.
+	 */
+	uint8_t add(uint8_t col, uint16_t x, uint16_t y);
+	/**
+	 * @brief functie push plaatst alle pixels uit de buffer op het scherm
+	 * @return de errorcode als het goed gaat wordt 0 gereturnt
+	 */
+	uint8_t push();
 
 };
 
